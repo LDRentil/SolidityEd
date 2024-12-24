457e6ba06dfbf39655161b069c0ae2b0fa6eb57b
Rentil <pylev2000@gmail.com>
2024-12-24 16:45:07 +0200

    voting contract, deploy test

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 contract Vote {
-
+// повторить мемори
     struct itsVote {
         string proposal;
         uint256 negativeVoice;
@@ -13,35 +13,35 @@ contract Vote {
 
     itsVote[] public votes;
 
-    mapping(uint256 => mapping(address => bool)) public hasVoted;
+    mapping(address => bool) public hasVoted;
 
     event newProposal(uint256 indexed id, string proposal, uint32 startVoteTime,uint32 endVoteTime);
 
-    function _createProposal(string _proposal) public {
-        uint256 id = uint256(keccak256(_proposal), block.timestamp);
+    function _createProposal(string memory _proposal) public {
+        uint256 id = uint256(keccak256(abi.encodePacked(_proposal, block.timestamp)));
         require(votes[id].startVoteTime == 0,"");
-        votes.push(Vote (id, _proposal, false,0,0,block.timestamp,block.timestamp+30));
-        newProposal(id,_proposal, false,0,0,block.timestamp,block.timestamp+30);
+        votes.push(itsVote (_proposal,0,0,uint32(block.timestamp),uint32(block.timestamp+30),false));
+        emit newProposal(id,_proposal,uint32(block.timestamp),uint32(block.timestamp+30));
     }
 
     function voteFor(uint256 _proposalId) public {
-        itsVote _vote = votes[_proposalId];
-        require(block.timestamp <= itsVote.startVoteTime,"Voting has not started yet.");
-        require(block.timestamp >= itsVote.endVoteTime,"The voting has already ended.");
+        itsVote memory _vote = votes[_proposalId];
+        require(block.timestamp <= votes[_proposalId].startVoteTime,"Voting has not started yet.");
+        require(block.timestamp >= votes[_proposalId].endVoteTime,"The voting has already ended.");
         require(!hasVoted[msg.sender],"Already voted");
         hasVoted[msg.sender];
-        itsVote.positiveVoice++;
-        hasVoted = true;
+        votes[_proposalId].positiveVoice++;
+        hasVoted[msg.sender] = true;
     }
 
     function voteAgainst(uint256 _proposalId) public {
-        itsVote _vote = votes[_proposalId];
-        require(block.timestamp <= itsVote.startVoteTime,"Voting has not started yet.");
-        require(block.timestamp >= itsVote.endVoteTime,"The voting has already ended.");
+        itsVote memory _vote = votes[_proposalId];
+        require(block.timestamp <= votes[_proposalId].startVoteTime,"Voting has not started yet.");
+        require(block.timestamp >= votes[_proposalId].endVoteTime,"The voting has already ended.");
         require(!hasVoted[msg.sender],"Already voted");
         hasVoted[msg.sender];
-        itsVote.negativeVoice++;
-        hasVoted = true;
+        votes[_proposalId].negativeVoice++;
+        hasVoted[msg.sender] = true;
     }
 
     function getProposal(uint256 _proposalId) public view returns (
